26ab7da96ad8b20d979aeff36f61b2f07f92562e
andylayzell <andy.layzell@gmail.com>
2015-05-05 17:17:47 +1000

    Script for restarting the search service
    Script for restarting the search service

---
+++ RestartSearchService.ps1
@@ -0,0 +1,6 @@
+﻿#####################################
+### Restart the osearch15 service ###
+#####################################
+Write-Host "Restarting SharePoint 14 Search Service (osearch14)"
+get-service osearch14 | restart-service
+Write-Host "SharePoint 14 Search Service (osearch15) successfully restarted"
