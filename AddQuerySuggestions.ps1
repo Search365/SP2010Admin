82b65ec0253313d56ffe2809599c3f6602013698
andylayzell <andy.layzell@gmail.com>
2015-07-09 15:45:08 +1000

    Adding a script for Query Suggestions
    This adds an item to the Query Suggestions.
    Also runs the timer jobs so that the query suggestions appear.

---
+++ AddQuerySuggestions.ps1
@@ -0,0 +1,6 @@
+﻿$searchapp = Get-SPEnterpriseSearchServiceApplication -Identity "FAST Query Service"
+ 
+New-SPEnterpriseSearchLanguageResourcePhrase -SearchApplication $searchapp -Language En-Us -Type QuerySuggestionAlwaysSuggest -Name "Testing"
+ 
+Start-SPTimerJob -Identity "Query Logging"
+Start-SPTimerJob -Identity "Prepare query suggestions"
